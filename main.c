b88644e58cda9700b90a8fe08ea907d3a0fe97c8
yc1016 <114291814+yc1016@users.noreply.github.com>
2022-10-27 23:29:06 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <REGX52.H>
+void Delay(unsigned int xms)		
+{
+	unsigned char i, j;
+	while (xms)
+	{
+	
+		i = 2;
+		j = 239;
+		do
+		{
+				while (--j);
+		} 	while (--i);
+		xms--;
+	}
+}//无用循环，耗时间
+void main()
+{
+		while(1)
+		{
+			if (P3_1==0)
+			{
+				Delay(20);
+				while (P3_1==0);
+				Delay(20);
+				P2_0=0;
+				Delay(500);
+				P2_0=1;
+				Delay(500);
+				P2_0=0;
+				Delay(500);
+				P2_0=1;
+				Delay(500);
+				P2_0=0;
+				Delay(500);
+				P2_0=1;
+				Delay(500);				
+		}
+	}
+
+
+
+
+}
